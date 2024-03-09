c87ee00e0f2283642ed94adb51d202b93f286e53
sverrirsig <svesig@gmail.com>
2024-03-09 13:48:58 +0000

    Add anonymous client

--- wrapper.go
+++ wrapper.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	NoonaBaseURL    = "https://api.noona.is"
-	NoonaAuthHeader = "HQ-ACCESS-KEY"
+	NoonaAuthHeader = "Authorization"
 )
 
 type ClientOptions struct {
@@ -27,11 +27,28 @@ func New(token string, options ...ClientOptions) (*ClientWithResponses, error) {
 	}
 
 	authHeader := WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
-		req.Header.Add(NoonaAuthHeader, token)
+		req.Header.Add(NoonaAuthHeader, "Bearer "+token)
 		return nil
 	})
 
 	c, err := NewClientWithResponses(opts.BaseURL, authHeader)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create client")
+	}
+
+	return c, nil
+}
+
+func NewAnonymous(options ...ClientOptions) (*ClientWithResponses, error) {
+	opts := ClientOptions{
+		BaseURL: NoonaBaseURL,
+	}
+
+	if len(options) > 0 {
+		opts = options[0]
+	}
+
+	c, err := NewClientWithResponses(opts.BaseURL)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create client")
 	}
